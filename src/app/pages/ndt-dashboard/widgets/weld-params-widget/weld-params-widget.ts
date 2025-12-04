c2de581cffeabd23878f40d5e1d3bc8b8378fadb
KokosAnanas <kurochkinrv@list.ru>
2025-12-05 00:30:46 +0300

    05.12.2025

--- src/app/pages/ndt-dashboard/widgets/weld-params-widget/weld-params-widget.ts
+++ src/app/pages/ndt-dashboard/widgets/weld-params-widget/weld-params-widget.ts
@@ -3,13 +3,16 @@ import { Card } from 'primeng/card';
 import { FormsModule } from '@angular/forms';
 import { InputNumber } from 'primeng/inputnumber';
 import { Select } from 'primeng/select';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-weld-params-widget',
-    imports: [Card, FormsModule, InputNumber, Select],
+    imports: [Card, FormsModule, InputNumber, Select, NgOptimizedImage],
     templateUrl: './weld-params-widget.html',
     styleUrl: './weld-params-widget.scss'
 })
 export class WeldParamsWidget {
     qualityLevels = ['A', 'B', 'C'];
+    weldingProcess = ['Ручная дуговая, полуавтоматическая', 'Автоматическая в защитных газах', 'Автоматическая под флюсом'];
+    joint = ['Стыковое', 'Угловое и нахлёсточное'];
 }
